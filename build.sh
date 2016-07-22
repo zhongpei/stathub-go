ac9e100c7b8d875823a766b277ff66761c53f91a
zhongpei <zhongpei@vip.qq.com>
2016-07-22 11:26:25 +0800

    fix

--- build.sh
+++ build.sh
@@ -2,7 +2,26 @@
 
 echo '+ building stathub start'
 [ ! -d stathub ] && mkdir stathub
-rm -rf tmp && mkdir tmp
+rm -rf tmp && mkdir tmp 
+cd tmp && mkdir static
+cd ..
+
+
+echo 'building the client'
+cd client
+go get
+go build
+cd ..
+mv client/client tmp
+
+
+cp CHANGS.md tmp
+cp CHANGS-ZH.md tmp
+cp LICENSE.md tmp
+cp README.md tmp
+cp README-ZH.md tmp
+cp VERSION tmp
+
 
 echo 'building the server'
 cd server
@@ -12,19 +31,6 @@ cd ..
 mv server/server tmp
 cp server/service tmp
 
-echo 'building the client'
-cd client
-go get
-go build
-cd ..
-mv client/client tmp
-
-cp CHANGS.md tmp
-cp CHANGS-ZH.md tmp
-cp LICENSE.md tmp
-cp README.md tmp
-cp README-ZH.md tmp
-cp VERSION tmp
 
 command_exists() {
     type "$1" &> /dev/null
@@ -36,10 +42,14 @@ if command_exists goupx; then
     goupx server client >/dev/null 2>&1
 fi
 
-echo 'packaging the server'
-tar zcf server_$(uname -m).tar.gz server service VERSION *.md
 echo 'packaging the client'
 tar zcf client_$(uname -m).tar.gz client VERSION *.md
+
+cp client_$(uname -m).tar.gz static
+
+echo 'packaging the server'
+tar zcf server_$(uname -m).tar.gz server service static VERSION *.md
+
 
 cd ..
 #cp tmp/server_$(uname -m).tar.gz tmp/client_$(uname -m).tar.gz stathub
